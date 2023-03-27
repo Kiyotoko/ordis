c16be4ae32a7b56cd8813e96a8e6fb6473ebf5e6
Karl Zschiebsch <karl.zschiebsch@gmail.com>
2023-03-27 16:35:48 +0200

    Add base

--- src/main/java/com/ordis/Bot.java
+++ src/main/java/com/ordis/Bot.java
@@ -1,14 +1,22 @@
 package com.ordis;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Consumer;
 
 import javax.annotation.Nonnull;
 
+import com.ordis.base.Issue;
+import com.ordis.base.IssueState;
+import com.ordis.game.Game.TikTakToe;
+
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.MessageBuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.internal.interactions.CommandDataImpl;
@@ -21,11 +29,20 @@ public class Bot {
                 event.reply("Why should I help you? Don't see a reason.").queue();
             },
 
+            // tik tak toe
+            new CommandDataImpl("tik-tak-toe", "starts a tik-tak-toe game"),
+            (SlashCommandInteractionEvent event) -> {
+                event.reply("I am starting a new game for you ...").queue();
+                new TikTakToe(event.getTextChannel(), event.getUser()).start();
+            },
             // privacy
             new CommandDataImpl("privacy", "returns importent privacy information"),
             (SlashCommandInteractionEvent event) -> {
                 event.reply("Privacy? Sounds like an interesting concept for me.").queue();
-            });
+            }//
+    );
+
+    private final Map<String, Issue> issues = new HashMap<>();
 
     @SuppressWarnings("null")
     protected void start(String token) throws Exception {
@@ -50,5 +67,37 @@ public class Bot {
                 }
             }
         }
+
+        @Override
+        public void onMessageReceived(@Nonnull MessageReceivedEvent event) {
+            if (event.getAuthor().isBot())
+                return;
+            Issue issue = new Issue(Bot.this, event.getTextChannel(), new MessageBuilder(
+                    "That could mean everything! React now to say: yeah Ordis, you are the best human exploiter!"),
+                    IssueState.OPEN) {
+                public void onMessageReactionAdd(@Nonnull MessageReactionAddEvent event) {
+                    System.out.println(event.getReaction());
+                    if (isValid()) {
+                        getBuilder().setContent("At least *SOMEONE* is proud of me!");
+                        setState(IssueState.CLOSED);
+                        edit(getBuilder().build());
+                        end();
+                    }
+                };
+            };
+            issue.start();
+        }
+
+        @Override
+        public void onMessageReactionAdd(@Nonnull MessageReactionAddEvent event) {
+            Issue issue = issues.get(event.getMessageId());
+            if (issue != null) {
+                issue.onMessageReactionAdd(event);
+            }
+        }
+    }
+
+    public Map<String, Issue> getIssues() {
+        return issues;
     }
 }
