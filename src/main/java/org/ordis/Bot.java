0c533dd100d24aa83e672a2995e30d24ce082fd2
Karl Zschiebsch <karl.zschiebsch@gmail.com>
2023-10-26 19:39:37 +0200

    Add Dockerfile

--- src/main/java/org/ordis/Bot.java
+++ src/main/java/org/ordis/Bot.java
@@ -6,13 +6,12 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import org.ordis.base.*;
+
 
 public class Bot {
 
 	public void start(String token) throws InterruptedException {
 		JDA jda = JDABuilder.createDefault(token).addEventListeners(new OrdisListenerAdapter()).build();
-		jda.updateCommands().addCommands(adapter.getData()).queue();
 		jda.awaitReady();
 
 		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
@@ -21,23 +20,10 @@ public class Bot {
 		}));
 	}
 
-	private final @Nonnull OrdisCommandAdapter adapter = new OrdisCommandAdapter();
-
-	protected class OrdisListenerAdapter extends ListenerAdapter {
+	protected static class OrdisListenerAdapter extends ListenerAdapter {
 		@Override
 		public void onSlashCommandInteraction(@Nonnull SlashCommandInteractionEvent event) {
-			OrdisDB.register(event.getUser());
-			adapter.call(event);
-			OrdisDB.select();
-		}
-	}
-
-	protected static class OrdisCommandAdapter extends CommandAdapter {
-
-		@Command(name = "help", description = "Gives help.")
-		@Override
-		public void help(SlashCommandInteractionEvent event) {
-			event.reply("No help available.").queue();
+			event.reply("You have used a slash command!").queue();
 		}
 	}
 }
